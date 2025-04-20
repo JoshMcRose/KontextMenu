1e2a0b4c135bb03707fcf081b1427f40eb9b755b
ubertrombone <josh_rose@alumni.baylor.edu>
2025-04-20 16:56:17 +0300

    Updates title.

--- app/src/desktopMain/kotlin/io/github/joshmcrose/Implementations.kt
+++ app/src/desktopMain/kotlin/io/github/joshmcrose/Implementations.kt
@@ -1,7 +1,7 @@
 package io.github.joshmcrose
 
 enum class Implementations(val full: String) {
-    M3("Material 3"),
+    M3("Material3"),
     M2("Material"),
     M3Dialog("Material 3 Dialog"),
     Custom("Custom")
