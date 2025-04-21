885ee10531be4fef9d6f5f262f6c05f5a81981e6
ubertrombone <josh_rose@alumni.baylor.edu>
2025-04-21 11:36:32 +0300

    Explicitly defines group and version in library build file.

--- kontextmenu/build.gradle.kts
+++ kontextmenu/build.gradle.kts
@@ -61,7 +61,7 @@ version = "0.1.0"
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
-    coordinates(group.toString(), "kontextmenu", version.toString())
+    coordinates("io.github.joshmcrose", "kontextmenu", "0.1.0")
 
     pom {
         name = "KontextMenu"
